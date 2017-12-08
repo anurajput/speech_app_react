6a94e5a187a42ca545482b4982cc6f9063cb6883
Anu <anurajput2017bytes@gmail.com>
2017-12-08 19:17:26 +0530

    updated speect test page

--- src/containers/SpeechTestPage.js
+++ src/containers/SpeechTestPage.js
@@ -42,23 +42,102 @@ class SpeechTestPage extends React.Component {
   constructor(props) {
     super(props);
 
+    //this.state = {   
+      //audioChunks: [],
+    //};
+
+    this.audioChunks = [];
+    this.rec         = null;
+    
+    this.abc = 12;//dummy
+
+    this.setupMediaDevice   = this.setupMediaDevice.bind(this);
+    this.initializeRecorder = this.initializeRecorder.bind(this);
+    this.startRecording     = this.startRecording.bind(this);
+    this.stopRecording      = this.stopRecording.bind(this);
+    this.playRecording      = this.playRecording.bind(this);
+    this.saveRecording      = this.saveRecording.bind(this);
 
 
-    var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    this.recognition = new SpeechRecognition();
-
-    console.log("recognition: " + this.recognition);
-
+    this.setupMediaDevice();
   }
+
+
+  setupMediaDevice() {
+    console.log('-- setupRecorder --');
+
+    //-------------------------------------
+    //   ensure getUserMedia is supported
+    //-------------------------------------
+    if (!navigator.mediaDevices) {
+      console.log("getUserMedia not supported on your browser!");
+      return;
+    }
+    if(!navigator.mediaDevices.getUserMedia) {
+      console.log("getUserMedia not supported on your browser!");
+      return;
+    }
+
+    console.log("GOOD: getUserMedia not supported on your browser!");
+
+    //----------------------------------------------------
+    // keep the this var in page, as in nested callback
+    // this cant be the page/current class/component
+    //----------------------------------------------------
+    var page = this;
+
+    // constraints - only audio needed for this app
+    navigator.mediaDevices.getUserMedia ({audio: true})
+      .then(function(stream) {
+        console.log("getUserMedia ok!");
+        page.initializeRecorder(stream);
+      })
+      .catch(function(err) {
+         console.log('The following gUM error occured: ' + err);
+      }
+    );
+    
+  }
+
+  initializeRecorder(stream) {
+    console.log("-- initializeRecorder --");
+
+    console.log("-- abc: " + this.abc);
+
+
+    var recordedAudio = document.getElementById("recordedAudio");
+    var audioDownload = document.getElementById("audioDownload");
+
+    var page = this;
+    this.rec = new MediaRecorder(stream);
+    this.rec.ondataavailable = e => {
+      page.audioChunks.push(e.data);
+      if (page.rec.state == "inactive"){
+        
+        let blob = new Blob(page.audioChunks,{type:'audio/x-mpeg-3'});
+        
+        recordedAudio.src       = URL.createObjectURL(blob);
+        recordedAudio.controls  = true;
+        recordedAudio.autoplay  = false;
+        
+        audioDownload.href      = recordedAudio.src;
+        audioDownload.download  = 'mp3';
+        audioDownload.innerHTML = 'download';
+      }
+    }//ondataavailable
+  
+  }//initializeRecorder
 
   startRecording() {
     console.log("-- startRecording --");
-    this.recognition.start();
+    this.audioChunks = [];
+    this.rec.start();
+    console.log("-- startRecording done");
   }
 
   stopRecording() {
     console.log("-- stopRecording --");
-    this.recognition.stop();
+    this.rec.stop();
   }
 
   playRecording() {
@@ -98,6 +177,8 @@ class SpeechTestPage extends React.Component {
                 STEP-1: <RaisedButton label="RECORD" primary={true} onClick={this.startRecording}/> <RaisedButton label="STOP" primary={true} onClick={this.stopRecording}/><br /><br /><br />
                 STEP-2: <RaisedButton label="PLAY" primary={true} onClick={this.playRecording}/><br /><br /><br />
                 STEP-3: <RaisedButton label="SAVE" primary={true} onClick={this.saveRecording}/><br /><br /><br />
+                <audio id="recordedAudio"></audio>
+                <a id="audioDownload"></a>
               </Paper>
             </div>
           </div>
